1e797265888509bb399768ee29514053be1aa2d8
Jennifer Wang Xue'Er <jenniferwxe@gmail.com>
2025-12-28 15:58:56 +0800

    feat[data-generation]: added more constant variables for data generation usage

--- data_generation/config.py
+++ data_generation/config.py
@@ -1,13 +1,16 @@
+from datetime import datetime
+
 # For testing:
-NUM_CUSTOMERS = 50
+NUM_CUSTOMERS = 100
 CONTROL_GROUP_PERCENTAGE = 0.5
-NUM_PRODUCTS = 5
+NUM_PRODUCTS = 100
 NUM_PHYSICAL_STORES = 5
-NUM_CAMPAIGNS = 10
+NUM_CAMPAIGNS = 20
 AUDIENCE_PERCENTAGE = 1
-MIN_REVIEWS = 5
-MAX_REVIEWS = 20
-NUM_CLICKSTREAMS = 200
+MIN_REVIEWS = 1
+MAX_REVIEWS = 2
+NUM_CLICKSTREAMS = 100
+PROBABILITY_OF_SESSION = 0.2
 # Actual Numbers:
 # NUM_CUSTOMERS = 50000
 # CONTROL_GROUP_PERCENTAGE = 0.1
@@ -18,6 +21,33 @@ NUM_CLICKSTREAMS = 200
 # MIN_REVIEWS = 20
 # MAX_REVIEWS = 100
 # NUM_CLICKSTREAMS = 10000000
+# PROBABILITY_OF_INACTIVE_SESSION =
+
+# customers #
+TARGET_SEGMENT = {
+    "New Customers": 0.2,
+    "Active Customers": 0.5,
+    "Churn Risk Customers": 0.1,
+    "High Spenders": 0.1,
+    "Budget Shoppers": 0.1,
+}
+
+MONTH_WEIGHTS_2024 = {
+    1: 0.5,
+    2: 0.6,
+    3: 0.7,
+    4: 0.8,
+    5: 0.9,
+    6: 1.0,
+    7: 1.1,
+    8: 1.2,
+    9: 1.3,
+    10: 1.4,
+    11: 1.8,  # 11.11
+    12: 2.0,  # 12.12 + Christmas
+}
+
+# products #
 
 CATEGORIES = [
     "Snacks",
@@ -25,6 +55,7 @@ CATEGORIES = [
     "Dairy & Eggs",
     "Frozen Food",
     "Fresh Produce",
+    "Pantry Staples",
     "Household Essentials",
     "Health & Beauty",
     "Baby Products",
@@ -41,52 +72,73 @@ CATEGORIES = [
 ]
 
 BRANDS = {
-    "Snacks": ["Lays", "Pringles", "Nature Valley", "KitKat", "Oreo"],
-    "Beverages": ["Coca Cola", "Pepsi", "Nestle", "Lipton", "Minute Maid"],
-    "Dairy & Eggs": ["Anchor", "Farmhouse", "Meadow Gold", "Marigold"],
-    "Frozen Food": ["Birds Eye", "Green Giant", "McCain"],
-    "Fresh Produce": ["Dole", "Chiquita", "Local Farms"],
-    "Household Essentials": ["Dettol", "Clorox", "Finish", "3M"],
-    "Health & Beauty": ["Nivea", "Colgate", "Dove", "L'Oreal"],
-    "Baby Products": ["Pampers", "Huggies", "Johnson's Baby"],
-    "Canned Goods": ["Del Monte", "Heinz", "Campbell's"],
-    "Personal Care": ["Pantene", "Head & Shoulders", "Gillette"],
-    "Meat & Seafood": ["Farmhouse", "OceanFresh", "Sea Best"],
-    "Bakery": ["Gardenia", "Bimbo", "Wonder Bread"],
-    "Cleaning Supplies": ["Ajax", "Mr Muscle", "Vanish"],
-    "Rice & Noodles": ["Tilda", "MamyPoko", "Maggi"],
-    "Breakfast Foods": ["Kellogg's", "Quaker", "Post"],
-    "Electronics & Appliances": ["Philips", "Samsung", "Xiaomi", "Dyson", "Panasonic"],
+    "Snacks": ["Lays", "Pringles", "Oreo", "Jack n Jill", "Ritz", "Nature Valley"],
+    "Beverages": ["Coca-Cola", "Pepsi", "Heaven & Earth", "Nestle", "Pokka"],
+    "Dairy & Eggs": ["Farmhouse", "Marigold", "Meiji", "Nestlé", "SCS"],
+    "Frozen Food": ["CP", "McCain", "Farmpride", "First Pride"],
+    "Fresh Produce": ["Dole", "Sunkist", "Zespri", "Local Farm"],
+    "Pantry Staples": ["Knife", "Lee Kum Kee", "Ayam Brand", "San Remo", "FairPrice"],
+    "Household Essentials": ["Kleenex", "FairPrice", "Hada", "Premier"],
+    "Health & Beauty": ["Colgate", "Dove", "Nivea", "L'Oreal", "Sensodyne"],
+    "Baby Products": ["Pampers", "Huggies", "MamyPoko", "Johnson's"],
+    "Canned Goods": ["Ayam Brand", "Campbell's", "Del Monte", "Heinz"],
+    "Personal Care": [
+        "Gillette",
+        "Schick",
+        "Lifebuoy",
+        "Dettol",
+        "Pantene",
+        "Head & Shoulders",
+    ],
+    "Meat & Seafood": ["Ben's Farm", "Sadia", "Ocean Fresh", "Farmhouse"],
+    "Bakery": ["Gardenia", "Sunshine", "Bimbo"],
+    "Cleaning Supplies": ["Mr Muscle", "Vanish", "Ajax", "Clorox"],
+    "Rice & Noodles": ["Royal Umbrella", "SongHe", "Maggi", "Koka"],
+    "Breakfast Foods": ["Kellogg's", "Quaker", "Post", "Nestlé"],
+    "Electronics & Appliances": [
+        "Philips",
+        "Xiaomi",
+        "Samsung",
+        "Panasonic",
+        "Dyson",
+    ],
     "Home & Living (Kitchenware, Storage, Bedding)": [
         "IKEA",
-        "Tefal",
         "Lock&Lock",
+        "Tefal",
         "Zojirushi",
         "HomeBasics",
     ],
     "Lifestyle & Recreation (Fitness, Toys, Travel)": [
+        "Decathlon",
         "Nike",
         "Adidas",
-        "Decathlon",
-        "LEGO",
         "Samsonite",
+        "LEGO",
     ],
 }
 
+
 CATEGORY_ITEMS = {
     "Snacks": ["Chips", "Biscuits", "Bars", "Cookies", "Nuts"],
-    "Beverages": ["Cola", "Juice", "Tea", "Coffee", "Soda"],
-    "Dairy & Eggs": ["Milk", "Cheese", "Butter", "Yogurt", "Cream"],
-    "Frozen Food": ["Peas", "Pizza", "Fries", "Fish Fingers", "Dumplings"],
+    "Beverages": ["Cola", "Juice", "Tea", "Coffee", "Soda", "Energy Drinks"],
+    "Dairy & Eggs": ["Milk", "Cheese", "Butter", "Yogurt", "Cream", "Eggs"],
+    "Frozen Food": ["Pizza", "Fries", "Dumplings", "Frozen Veggies", "Ice Cream"],
     "Fresh Produce": ["Apples", "Bananas", "Carrots", "Tomatoes", "Spinach"],
-    "Household Essentials": ["Detergent", "Soap", "Cleaner", "Spray", "Wipes"],
-    "Health & Beauty": ["Lotion", "Shampoo", "Toothpaste", "Soap", "Conditioner"],
+    "Pantry Staples": ["Cooking Oil", "Salt", "Sugar", "Flour", "Sauces", "Seasoning"],
+    "Household Essentials": [
+        "Toilet Paper",
+        "Trash Bags",
+        "Detergent Pods",
+        "Soap Refills",
+    ],
+    "Health & Beauty": ["Toothpaste", "Lotion", "Shampoo", "Conditioner", "Body Wash"],
     "Baby Products": ["Diapers", "Wipes", "Powder", "Cream", "Formula"],
     "Canned Goods": ["Tomatoes", "Corn", "Beans", "Tuna", "Soup"],
-    "Personal Care": ["Shampoo", "Conditioner", "Soap", "Deodorant", "Gel"],
-    "Meat & Seafood": ["Chicken", "Beef", "Fish", "Prawns", "Bacon"],
+    "Personal Care": ["Deodorant", "Shaving Foam", "Razors", "Hand Sanitizer"],
+    "Meat & Seafood": ["Chicken", "Beef", "Fish", "Prawns", "Salmon", "Bacon"],
     "Bakery": ["Bread", "Cake", "Buns", "Croissant", "Muffin"],
-    "Cleaning Supplies": ["Detergent", "Bleach", "Spray", "Scrub", "Wipes"],
+    "Cleaning Supplies": ["Bleach", "All-purpose Spray", "Scrub Brush", "Wipes"],
     "Rice & Noodles": ["Rice", "Pasta", "Noodles", "Vermicelli", "Spaghetti"],
     "Breakfast Foods": ["Cereal", "Oats", "Granola", "Muesli", "Porridge"],
     "Electronics & Appliances": [
@@ -145,6 +197,9 @@ POSITIVE_CATEGORY_DESCRIPTORS = {
     "Fresh Produce": {
         "taste": ["fresh", "sweet", "bitter", "tangy", "juicy", "earthy"],
         "texture": ["crisp", "firm", "soft", "juicy", "fibrous"],
+    },
+    "Pantry Staples": {
+        "taste": ["fresh", "rich", "consistent", "high-quality"],
     },
     "Household Essentials": {
         "texture": ["smooth", "abrasive", "soft", "durable"],
@@ -224,6 +279,9 @@ NEUTRAL_CATEGORY_DESCRIPTORS = {
     "Fresh Produce": {
         "taste": ["average", "fresh enough", "ordinary", "mild", "typical"],
         "texture": ["firm enough", "soft", "average", "acceptable", "slightly crisp"],
+    },
+    "Pantry Staples": {
+        "taste": ["standard", "typical", "average-quality"],
     },
     "Household Essentials": {
         "texture": ["standard", "soft", "average", "durable enough"],
@@ -316,6 +374,9 @@ NEGATIVE_CATEGORY_DESCRIPTORS = {
     "Fresh Produce": {
         "taste": ["bitter", "sour", "mealy", "watery", "off", "bland"],
         "texture": ["wilted", "soft in a bad way", "fibrous", "mushy", "dry"],
+    },
+    "Pantry Staples": {
+        "taste": ["tasteless", "watery", "low-quality", "inconsistent", "not fresh"],
     },
     "Household Essentials": {
         "texture": ["rough", "flimsy", "cheap", "scratches easily", "fragile"],
@@ -668,6 +729,8 @@ NEGATIVE_VERBS = [
 
 NEUTRAL_VERBS = ["found", "tried", "tested", "used", "experienced"]
 
+# physical stores #
+
 MALL_TO_AREA = {
     "Waterway Point": "Punggol",
     "Junction 8": "Bishan",
@@ -720,4 +783,389 @@ MALL_TO_AREA = {
     "Marine Parade Central": "Marine Parade",
     "The Grandstand": "Bukit Timah",
     "Hillion Mall": "Bukit Panjang",
+}
+
+# campaigns #
+CATEGORY_CAMPAIGN_PROBABILITY = {
+    "Snacks": 0.15,
+    "Beverages": 0.12,
+    "Dairy & Eggs": 0.05,
+    "Frozen Food": 0.04,
+    "Fresh Produce": 0.02,
+    "Pantry Staples": 0.01,
+    "Household Essentials": 0.01,
+    "Health & Beauty": 0.05,
+    "Baby Products": 0.03,
+    "Canned Goods": 0.02,
+    "Personal Care": 0.05,
+    "Meat & Seafood": 0.03,
+    "Bakery": 0.04,
+    "Cleaning Supplies": 0.01,
+    "Rice & Noodles": 0.01,
+    "Breakfast Foods": 0.03,
+    "Electronics & Appliances": 0.10,
+    "Home & Living (Kitchenware, Storage, Bedding)": 0.05,
+    "Lifestyle & Recreation (Fitness, Toys, Travel)": 0.08,
+}
+
+CATEGORY_CODE_MAP = {
+    "Snacks": "SNACK",
+    "Beverages": "BEV",
+    "Dairy & Eggs": "DAIRY",
+    "Frozen Food": "FROZEN",
+    "Fresh Produce": "FRESH",
+    "Pantry Staples": "PANTRY",
+    "Household Essentials": "ESSEN",
+    "Health & Beauty": "HEALTH",
+    "Baby Products": "BABY",
+    "Canned Goods": "CANNED",
+    "Personal Care": "CARE",
+    "Meat & Seafood": "MEAT",
+    "Bakery": "BAKE",
+    "Cleaning Supplies": "CLEAN",
+    "Rice & Noodles": "RICE",
+    "Breakfast Foods": "BFAST",
+    "Electronics & Appliances": "ELEC",
+    "Home & Living (Kitchenware, Storage, Bedding)": "H&L",
+    "Lifestyle & Recreation (Fitness, Toys, Travel)": "L&R",
+}
+
+# seasonal dates for 2024
+SEASONAL_DATES = {
+    "CNY": (datetime(2024, 2, 10), datetime(2024, 2, 17)),
+    "Christmas": (datetime(2024, 12, 25), datetime(2024, 12, 25)),
+    "Black Friday": (datetime(2024, 11, 29), datetime(2024, 11, 29)),
+    "1111": (datetime(2024, 11, 11), datetime(2024, 11, 11)),
+    "1212": (datetime(2024, 12, 12), datetime(2024, 12, 12)),
+}
+
+SEASON_PEAK_CATEGORIES = {
+    "CNY": [
+        "Beverages",
+        "Frozen Food",
+        "Lifestyle & Recreation",
+        "Meat & Seafood",
+        "Rice & Noodles",
+        "Snacks",
+    ],
+    "Christmas": [
+        "Bakery",
+        "Beverages",
+        "Dairy & Eggs",
+        "Electronics & Appliances",
+        "Lifestyle & Recreation (Fitness, Toys, Travel)",
+        "Meat & Seafood",
+    ],
+    "Black Friday": [
+        "Electronics & Appliances",
+        "Home & Living (Kitchenware, Storage, Bedding)",
+        "Lifestyle & Recreation (Fitness, Toys, Travel)",
+    ],
+    "1111": [
+        "Electronics & Appliances",
+        "Home & Living (Kitchenware, Storage, Bedding)",
+        "Lifestyle & Recreation (Fitness, Toys, Travel)",
+    ],
+    "1212": [
+        "Electronics & Appliances",
+        "Home & Living (Kitchenware, Storage, Bedding)",
+        "Lifestyle & Recreation (Fitness, Toys, Travel)",
+    ],
+}
+
+SEASON_CODE_MAP = {
+    "CNY": "CNY",
+    "Christmas": "XMAS",
+    "Black Friday": "BF",
+    "1111": "1111",
+    "1212": "1212",
+}
+
+# clickstreams #
+
+CAMPAIGN_SESSION_FREQUENCY = {
+    "treatment": 1.3,
+    "control": 1,
+    "non-campaign": 1,
+}
+
+CATEGORY_FREQUENCY = {
+    "Snacks": 0.09,
+    "Beverages": 0.07,
+    "Dairy & Eggs": 0.11,
+    "Frozen Food": 0.05,
+    "Fresh Produce": 0.14,
+    "Pantry Staples": 0.10,
+    "Household Essentials": 0.04,
+    "Health & Beauty": 0.04,
+    "Baby Products": 0.02,
+    "Canned Goods": 0.03,
+    "Personal Care": 0.04,
+    "Meat & Seafood": 0.05,
+    "Bakery": 0.03,
+    "Cleaning Supplies": 0.04,
+    "Rice & Noodles": 0.05,
+    "Breakfast Foods": 0.05,
+    "Electronics & Appliances": 0.02,
+    "Home & Living (Kitchenware, Storage, Bedding)": 0.04,
+    "Lifestyle & Recreation (Fitness, Toys, Travel)": 0.04,
+}
+
+# fix this later
+HOUR_WEIGHTS = {
+    "Midnight": (0, 6, 0.05),  # 12am–6am
+    "Daytime": (6, 18, 0.25),  # 6am–6pm
+    "Evening": (18, 22, 0.6),  # 6pm–10pm
+    "Late": (22, 24, 0.1),  # 10pm-12am
+}
+
+DAY_WEIGHTS = {"Weekday": 0.65, "Weekend": 0.35}
+
+PAYDAY_BOOST = 2.5  # relative likelihood
+
+SEASONAL_UPLIFT = {
+    "Daytime": {
+        "extra_events": (0, 1),
+        "session_mult": (0.95, 1.05),
+        "atc_mult": (0.98, 1.02),
+        "checkout_mult": (0.98, 1.02),
+        "conversion_mult": (0.98, 1.02),
+    },
+    "Evening": {
+        "extra_events": (1, 3),
+        "session_mult": (1.15, 1.35),
+        "atc_mult": (1.05, 1.12),
+        "checkout_mult": (1.05, 1.1),
+        "conversion_mult": (1.05, 1.12),
+    },
+    "Weekday": {
+        "extra_events": (0, 1),
+        "session_mult": (0.95, 1.05),
+        "atc_mult": (1.08, 1.02),
+        "checkout_mult": (1.08, 1.12),
+        "conversion_mult": (1.08, 1.02),
+    },
+    "Weekend": {
+        "extra_events": (1, 3),
+        "session_mult": (1.2, 1.45),
+        "atc_mult": (1.1, 1.2),
+        "checkout_mult": (1.08, 1.15),
+        "conversion_mult": (1.1, 1.18),
+    },
+    "Payday": {
+        "extra_events": (2, 4),
+        "session_mult": (1.15, 1.3),
+        "atc_mult": (1.15, 1.25),
+        "checkout_mult": (1.12, 1.2),
+        "conversion_mult": (1.15, 1.3),
+    },
+    "Payday spillover": {
+        "extra_events": (1, 3),
+        "session_mult": (1.1, 1.2),
+        "atc_mult": (1.1, 1.2),
+        "checkout_mult": (1.08, 1.15),
+        "conversion_mult": (1.1, 1.18),
+    },
+    "CNY": {
+        "extra_events": (3, 6),
+        "session_mult": (1.25, 1.5),
+        "atc_mult": (1.2, 1.4),
+        "checkout_mult": (1.3, 1.6),
+        "conversion_mult": (1.5, 2.2),
+    },
+    "Christmas": {
+        "extra_events": (3, 6),
+        "session_mult": (1.3, 1.55),
+        "atc_mult": (1.25, 1.5),
+        "checkout_mult": (1.4, 1.8),
+        "conversion_mult": (1.6, 2.5),
+    },
+    "1111": {
+        "extra_events": (5, 10),
+        "session_mult": (1.6, 2),
+        "atc_mult": (1.5, 2),
+        "checkout_mult": (1.8, 2.5),
+        "conversion_mult": (2, 3.5),
+    },
+    "1212": {
+        "extra_events": (5, 10),
+        "session_mult": (1.6, 2),
+        "atc_mult": (1.5, 2),
+        "checkout_mult": (1.8, 2.5),
+        "conversion_mult": (2, 3.5),
+    },
+}
+
+SEGMENT_CATEGORY_BIAS = {
+    "New Customers": {
+        "Snacks": 1.4,
+        "Beverages": 1.3,
+    },
+    "High Spenders": {
+        "Electronics & Appliances": 1.8,
+        "Home & Living (Kitchenware, Storage, Bedding)": 1.5,
+        "Lifestyle & Recreation (Fitness, Toys, Travel)": 1.4,
+    },
+    "Budget Shoppers": {
+        "Canned Goods": 1.5,
+        "Dairy & Eggs": 1.4,
+        "Frozen Food": 1.3,
+        "Household Essentials": 1.4,
+        "Pantry Staples": 1.3,
+        "Rice & Noodles": 1.3,
+    },
+}
+
+SEGMENT_SESSION_FREQUENCY = {
+    "New Customers": 1.2,
+    "Active Customers": 1.4,
+    "Churn Risk Customers": 0.3,
+    "High Spenders": 1,
+    "Budget Shoppers": 1.1,
+}
+
+REFERRER_DISTRIBUTION = {
+    "google.com": 0.55,
+    "facebook.com": 0.2,
+    "instagram.com": 0.06,
+    "tiktok.com": 0.05,
+    "youtube.com": 0.04,
+    "direct": 0.04,
+    "email_campaign": 0.03,
+    "telegram.org": 0.01,
+    "shopee.sg": 0.01,
+    "lazada.sg": 0.01,
+}
+
+LANDING_PAGE_BEHAVIOUR = {
+    "direct": {
+        "Home View": 0.70,
+        "Search View": 0.30,
+    },
+    "email_campaign": {
+        "Home View": 0.85,
+        "Category View": 0.15,
+    },
+    "google.com": {
+        "Search View": 0.60,
+        "Home View": 0.20,
+        "Product View": 0.20,
+    },
+    "facebook.com": {
+        "Product View": 0.80,
+        "Home View": 0.20,
+    },
+    "instagram.com": {
+        "Product View": 0.80,
+        "Home View": 0.20,
+    },
+    "tiktok.com": {
+        "Product View": 0.80,
+        "Home View": 0.20,
+    },
+    "youtube.com": {
+        "Product View": 0.80,
+        "Home View": 0.20,
+    },
+    "shopee.sg": {
+        "Product View": 0.80,
+        "Home View": 0.20,
+    },
+    "lazada.sg": {
+        "Product View": 0.80,
+        "Home View": 0.20,
+    },
+    "telegram.org": {
+        "Product View": 0.80,
+        "Home View": 0.20,
+    },
+}
+
+TIME_ON_PAGE = {
+    "Home View": (3, 8, 25),
+    "Category View": (5, 12, 40),
+    "Search View": (5, 15, 35),
+    "Product View": (10, 25, 80),
+    "Add to Cart": (3, 10, 30),
+    "Cart View": (5, 20, 60),
+    "Remove from Cart": (2, 6, 20),
+    "Checkout Start": (10, 30, 120),
+    "Payment Attempt": (5, 40, 180),
+    "Payment Successful": (2, 4, 10),
+    "Payment Failed": (2, 6, 30),
+}
+
+VALID_EVENT_TRANSITIONS = {
+    "Home View": {
+        "Product View": 0.4,
+        "Category View": 0.3,
+        "Search View": 0.25,
+        "Cart View": 0.05,
+    },
+    "Category View": {
+        "Product View": 0.45,
+        "Search View": 0.3,
+        "Home View": 0.2,
+        "Cart View": 0.05,
+    },
+    "Search View": {
+        "Product View": 0.5,
+        "Category View": 0.3,
+        "Home View": 0.2,
+    },
+    "Product View": {
+        "Category View": 0.35,
+        "Home View": 0.3,
+        "Search View": 0.15,
+        "Add to Cart": 0.15,
+        "Cart View": 0.05,
+    },
+    "Add to Cart": {
+        "Cart View": 0.4,
+        "Product View": 0.3,
+        "Category View": 0.2,
+        "Remove from Cart": 0.1,
+    },
+    "Cart View": {
+        "Checkout Start": 0.6,
+        "Remove from Cart": 0.2,
+        "Product View": 0.1,
+        "Category View": 0.1,
+    },
+    "Remove from Cart": {
+        "Cart View": 0.5,
+        "Product View": 0.2,
+        "Category View": 0.2,
+        "Home View": 0.1,
+    },
+    "Checkout Start": {
+        "Payment Attempt": 0.9,
+        "Remove from Cart": 0.1,
+    },
+    "Payment Attempt": {
+        "Payment Successful": 0.8,
+        "Payment Failed": 0.2,
+    },
+    "Payment Successful": {
+        "Home View": 0.2,
+    },
+    "Payment Failed": {
+        "Home View": 0.7,
+        "Payment Attempt": 0.3,
+    },
+}
+
+SEARCH_TERMS = []
+SEARCH_TERMS.extend(CATEGORIES)
+for brand_list in BRANDS.values():
+    SEARCH_TERMS.extend(brand_list)
+for item_list in CATEGORY_ITEMS.values():
+    SEARCH_TERMS.extend(item_list)
+
+CART_SIZE_BY_SEGMENT = {
+    "New Customers": 0.5,  # most will have 0 or 1 item
+    "Active Customers": 1.5,  # average 1–2 items
+    "High Spenders": 3.0,  # average 2–4 items, sometimes bigger
+    "Budget Shoppers": 2.0,  # small-medium cart, focused on staples
+    "Churn Risk Customers": 0.7,  # mostly empty
 }
